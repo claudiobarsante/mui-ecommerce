714ab8f078b41f995c7139ec852f91e925df5e65
Claudio Barsante <clbmribas@gmail.com>
2023-03-14 14:31:39 -0300

    tests: add tests to <FormSignIn/>

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,9 +1,5 @@
 import SignInTemplate from 'templates/SignIn';
 
 export default function SignIn() {
-  return (
-    <div style={{ border: '1px solid green', width: '100vw' }}>
-      <SignInTemplate />;
-    </div>
-  );
+  return <SignInTemplate />;
 }
