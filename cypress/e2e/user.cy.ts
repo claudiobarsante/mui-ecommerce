9ff947e9b8591d9400323eb9ab0a7f572d6cf46c
Claudio Barsante <clbmribas@gmail.com>
2023-03-21 16:21:06 -0300

    test: add test to check erros when user/password is wrong

--- cypress/e2e/user.cy.ts
+++ cypress/e2e/user.cy.ts
@@ -1,10 +1,11 @@
-import { cy, Cypress, expect, describe, it } from 'local-cypress';
+import { cy, Cypress, expect, describe, it, beforeEach } from 'local-cypress';
 
 describe('User', () => {
-  it('should sign in and sign out', () => {
+  beforeEach(() => {
     cy.visit('/sign-in');
     cy.url().should('contain', Cypress.config().baseUrl + '/sign-in');
-
+  });
+  it('should sign in and sign out', () => {
     cy.get('#email').type('fake@gmail.com');
     cy.get('#password').type('123456');
 
@@ -20,5 +21,36 @@ describe('User', () => {
     );
 
     // todo: implement signout
+  });
+
+  it('should show erros when user/password is wrong', () => {
+    // -- check for am invalid email
+    cy.get('#email').type('fakegmail.com');
+    cy.get('#password').type('123456');
+    cy.findByRole('button', { name: /Sign in/i }).click();
+
+    cy.get('#email-helper-text').should(
+      'have.text',
+      '"email" must be a valid email'
+    );
+    // -- check for invalid length password
+    cy.get('#email').clear();
+    cy.get('#email').type('fake@gmail.com');
+    cy.get('#password').clear();
+    cy.get('#password').type('12');
+    cy.findByRole('button', { name: /Sign in/i }).click();
+
+    cy.get('#component-error-text').should(
+      'have.text',
+      '"password" length must be at least 6 characters long'
+    );
+    // -- check for an invalid username or password
+    cy.get('#email').clear();
+    cy.get('#email').type('fake@gmail.com');
+    cy.get('#password').clear();
+    cy.get('#password').type('wrong-password');
+    cy.findByRole('button', { name: /Sign in/i }).click();
+
+    cy.findByText('username or password is invalid').should('exist');
   });
 });
